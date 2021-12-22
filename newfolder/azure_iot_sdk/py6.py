b40b44915598e877dc4d242158321ae394af5e87
SOUL8456 <95904614+SOUL8456@users.noreply.github.com>
2021-12-22 10:48:12 +0530

    Add files via upload

--- newfolder/azure_iot_sdk/py6.py
+++ newfolder/azure_iot_sdk/py6.py
@@ -1,36 +1,31 @@
-import os
-import asyncio
-from azure.iot.device.aio import IoTHubDeviceClient
-import time
 import random
 import string
-
-
+import time
+import asyncio
+from azure.iot.device.aio import IoTHubDeviceClient
 
 
 async def main():
-    conn_str = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
+    conn_str ="HostName=IoTServerTesting.azure-devices.net;DeviceId=training;SharedAccessKey=VMY8TQTg1zRqo8ey9vWLLgW8a7PzuSDobrPgtfH6Lew="
 
     device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
 
     await device_client.connect()
 
-letters=5
-async def Key_generator(letters):
-    while True:
-        str1=''.join(random.choices(string.ascii_letters+string.digits,k=letters))
-        str2=''.join(random.choices(string.ascii_letters+string.digits,k=letters))
-        str3=str1+str2
-        print(str3)
-        time.sleep(5)
+    for i in range(50):
+        str1 = ''.join(random.choices(string.ascii_letters + string.digits, k=5))
+        str2 = ''.join(random.choices(string.ascii_letters + string.digits, k=5))
+        str3 = str1 + str2
+        print("Sending message...",str3)
+        time.sleep(2)
         await device_client.send_message("This is a message that is being sent")
         print("Message successfully sent!")
 
         await device_client.shutdown()
 
+
 if __name__ == "__main__":
     asyncio.run(main())
-
 
 
 
